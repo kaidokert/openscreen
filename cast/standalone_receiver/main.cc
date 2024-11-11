msft-mirror-aosp/platform.external.openscreen

commit 4f27c4f1698522dfcea36dca948a13e2eaf4c26c

[Infra] Add Windows build support
This patch adds rudimentary windows build support. A complete
platform implementation is not included, so not all targets
are added to `gn_all` yet.

Bug: 236163082
Change-Id: I4f6d81bb0babb56a94e7c24777e4c572714d1ade
Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/5490382
Commit-Queue: Jordan Bayles <jophba@chromium.org>
Reviewed-by: Mark Foltz <mfoltz@chromium.org>

cast/standalone_receiver/main.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <getopt.h>
-
 #include <algorithm>
 #include <iostream>
 #include <memory>
@@ -21,6 +19,7 @@
 #include "platform/impl/platform_client_posix.h"
 #include "platform/impl/task_runner.h"
 #include "platform/impl/text_trace_logging_platform.h"
+#include "third_party/getopt/getopt.h"
 #include "util/chrono_helpers.h"
 #include "util/string_util.h"
 #include "util/stringprintf.h"
@@ -123,7 +122,7 @@ int RunStandaloneReceiver(int argc, char* argv[]) {
   // between all Open Screen executables. If it is a platform feature
   // being exposed, consider if it applies to the standalone receiver,
   // standalone sender, osp demo, and test_main argument options.
-  const struct option kArgumentOptions[] = {
+  const get_opt::option kArgumentOptions[] = {
       {"private-key", required_argument, nullptr, 'p'},
       {"developer-certificate", required_argument, nullptr, 'd'},
       {"generate-credentials", no_argument, nullptr, 'g'},

cast/standalone_sender/main.cc
@@ -5,8 +5,6 @@
 #include "platform/impl/logging.h"
 
 #if defined(CAST_STANDALONE_SENDER_HAVE_EXTERNAL_LIBS)
-#include <getopt.h>
-
 #include <cinttypes>
 #include <cstdio>
 #include <cstring>
@@ -27,6 +25,7 @@
 #include "platform/impl/platform_client_posix.h"
 #include "platform/impl/task_runner.h"
 #include "platform/impl/text_trace_logging_platform.h"
+#include "third_party/getopt/getopt.h"
 #include "util/chrono_helpers.h"
 #include "util/stringprintf.h"
 
@@ -109,17 +108,16 @@ int StandaloneSenderMain(int argc, char* argv[]) {
   // being exposed, consider if it applies to the standalone receiver,
   // standalone sender, osp demo, and test_main argument options.
   const struct option kArgumentOptions[] = {
-    {"max-bitrate", required_argument, nullptr, 'm'},
-    {"no-looping", no_argument, nullptr, 'n'},
-    {"developer-certificate", required_argument, nullptr, 'd'},
-    {"android-hack", no_argument, nullptr, 'a'},
-    {"remoting", no_argument, nullptr, 'r'},
-    {"tracing", no_argument, nullptr, 't'},
-    {"verbose", no_argument, nullptr, 'v'},
-    {"help", no_argument, nullptr, 'h'},
-    {"codec", required_argument, nullptr, 'c'},
-    {nullptr, 0, nullptr, 0}
-  };
+      {"max-bitrate", required_argument, nullptr, 'm'},
+      {"no-looping", no_argument, nullptr, 'n'},
+      {"developer-certificate", required_argument, nullptr, 'd'},
+      {"android-hack", no_argument, nullptr, 'a'},
+      {"remoting", no_argument, nullptr, 'r'},
+      {"tracing", no_argument, nullptr, 't'},
+      {"verbose", no_argument, nullptr, 'v'},
+      {"help", no_argument, nullptr, 'h'},
+      {"codec", required_argument, nullptr, 'c'},
+      {nullptr, 0, nullptr, 0}};
 
   int max_bitrate = kDefaultMaxBitrate;
   bool should_loop_video = true;

osp/demo/osp_demo.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <getopt.h>
 #include <poll.h>
 #include <signal.h>
 #include <unistd.h>
@@ -35,6 +34,7 @@
 #include "platform/impl/platform_client_posix.h"
 #include "platform/impl/task_runner.h"
 #include "platform/impl/text_trace_logging_platform.h"
+#include "third_party/getopt/getopt.h"
 #include "third_party/tinycbor/src/src/cbor.h"
 #include "util/trace_logging.h"
 
@@ -573,7 +573,7 @@ InputArgs GetInputArgs(int argc, char** argv) {
   // between all Open Screen executables. If it is a platform feature
   // being exposed, consider if it applies to the standalone receiver,
   // standalone sender, osp demo, and test_main argument options.
-  const struct option kArgumentOptions[] = {
+  const get_opt::option kArgumentOptions[] = {
       {"tracing", no_argument, nullptr, 't'},
       {"verbose", no_argument, nullptr, 'v'},
       {"help", no_argument, nullptr, 'h'},

tools/cddl/main.cc
@@ -4,17 +4,27 @@
 
 #include <fcntl.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/stat.h>
 
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
+#if defined(_WIN32)
+#include <io.h>
+#endif
+
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+#include <unistd.h>
+#endif
+
 #include "tools/cddl/codegen.h"
 #include "tools/cddl/logging.h"
 #include "tools/cddl/parse.h"
 #include "tools/cddl/sema.h"
+
+namespace {
 
 std::string ReadEntireFile(const std::string& filename) {
   std::ifstream input(filename);
@@ -100,6 +110,35 @@ CommandLineArguments ParseCommandLineArguments(int argc, char** argv) {
   return result;
 }
 
+void CloseFile(int fd) {
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+  close(fd);
+#elif defined(_WIN32)
+  _close(fd);
+#endif
+}
+
+struct File {
+  explicit File(int descriptor) : descriptor(descriptor) {}
+  ~File() { CloseFile(descriptor); }
+
+  int descriptor;
+};
+
+File OpenFile(const char* filename) {
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+  return File(open(filename, O_CREAT | O_TRUNC | O_WRONLY,
+                   S_IRUSR | S_IWUSR | S_IRGRP));
+#elif defined(_WIN32)
+  int fd = -1;
+  _sopen_s(&fd, filename, O_CREAT | O_TRUNC | O_WRONLY, _SH_DENYNO,
+           _S_IREAD | _S_IWRITE);
+  return File(fd);
+#endif
+}
+
+}  // namespace
+
 int main(int argc, char** argv) {
   // Parse and validate all cmdline arguments.
   CommandLineArguments args = ParseCommandLineArguments(argc, argv);
@@ -123,18 +162,16 @@ int main(int argc, char** argv) {
 
   // Validate and open the provided header file.
   std::string header_filename = args.gen_dir + "/" + args.header_filename;
-  int header_fd = open(header_filename.c_str(), O_CREAT | O_TRUNC | O_WRONLY,
-                       S_IRUSR | S_IWUSR | S_IRGRP);
-  if (header_fd == -1) {
+  const File header_file = OpenFile(header_filename.c_str());
+  if (header_file.descriptor == -1) {
     std::cerr << "failed to open " << args.header_filename << std::endl;
     return 1;
   }
 
   // Validate and open the provided output source file.
   std::string cc_filename = args.gen_dir + "/" + args.cc_filename;
-  int cc_fd = open(cc_filename.c_str(), O_CREAT | O_TRUNC | O_WRONLY,
-                   S_IRUSR | S_IWUSR | S_IRGRP);
-  if (cc_fd == -1) {
+  const File cc_file = OpenFile(cc_filename.c_str());
+  if (cc_file.descriptor == -1) {
     std::cerr << "failed to open " << args.cc_filename << std::endl;
     return 1;
   }
@@ -183,70 +220,67 @@ int main(int argc, char** argv) {
   // Create the C++ files from the Symbol table.
 
   Logger::Log("Writing Header prologue...");
-  if (!WriteHeaderPrologue(header_fd, args.header_filename)) {
+  if (!WriteHeaderPrologue(header_file.descriptor, args.header_filename)) {
     Logger::Error("WriteHeaderPrologue failed");
     return 1;
   }
   Logger::Log("Successfully wrote header prologue!");
 
   Logger::Log("Writing type definitions...");
-  if (!WriteTypeDefinitions(header_fd, &cpp_result.second)) {
+  if (!WriteTypeDefinitions(header_file.descriptor, &cpp_result.second)) {
     Logger::Error("WriteTypeDefinitions failed");
     return 1;
   }
   Logger::Log("Successfully wrote type definitions!");
 
   Logger::Log("Writing function declaration...");
-  if (!WriteFunctionDeclarations(header_fd, &cpp_result.second)) {
+  if (!WriteFunctionDeclarations(header_file.descriptor, &cpp_result.second)) {
     Logger::Error("WriteFunctionDeclarations failed");
     return 1;
   }
   Logger::Log("Successfully wrote function declarations!");
 
   Logger::Log("Writing header epilogue...");
-  if (!WriteHeaderEpilogue(header_fd, args.header_filename)) {
+  if (!WriteHeaderEpilogue(header_file.descriptor, args.header_filename)) {
     Logger::Error("WriteHeaderEpilogue failed");
     return 1;
   }
   Logger::Log("Successfully wrote header epilogue!");
 
   Logger::Log("Writing source prologue...");
-  if (!WriteSourcePrologue(cc_fd, args.header_filename)) {
+  if (!WriteSourcePrologue(cc_file.descriptor, args.header_filename)) {
     Logger::Error("WriteSourcePrologue failed");
     return 1;
   }
   Logger::Log("Successfully wrote source prologue!");
 
   Logger::Log("Writing encoders...");
-  if (!WriteEncoders(cc_fd, &cpp_result.second)) {
+  if (!WriteEncoders(cc_file.descriptor, &cpp_result.second)) {
     Logger::Error("WriteEncoders failed");
     return 1;
   }
   Logger::Log("Successfully wrote encoders!");
 
   Logger::Log("Writing decoders...");
-  if (!WriteDecoders(cc_fd, &cpp_result.second)) {
+  if (!WriteDecoders(cc_file.descriptor, &cpp_result.second)) {
     Logger::Error("WriteDecoders failed");
     return 1;
   }
   Logger::Log("Successfully wrote decoders!");
 
   Logger::Log("Writing equality operators...");
-  if (!WriteEqualityOperators(cc_fd, &cpp_result.second)) {
+  if (!WriteEqualityOperators(cc_file.descriptor, &cpp_result.second)) {
     Logger::Error("WriteStructEqualityOperators failed");
     return 1;
   }
   Logger::Log("Successfully wrote equality operators!");
 
   Logger::Log("Writing source epilogue...");
-  if (!WriteSourceEpilogue(cc_fd)) {
+  if (!WriteSourceEpilogue(cc_file.descriptor)) {
     Logger::Error("WriteSourceEpilogue failed");
     return 1;
   }
   Logger::Log("Successfully wrote source epilogue!");
-
-  close(header_fd);
-  close(cc_fd);
   Logger::Log("SUCCESSFULLY COMPLETED ALL OPERATIONS");
 
   return 0;

BUILD.gn
@@ -35,31 +35,37 @@ group("gn_all") {
     "cast/common:public",
     "cast/receiver:channel",
     "cast/sender:channel",
-    "cast/streaming:receiver",
-    "cast/streaming:sender",
-    "discovery:dnssd",
-    "discovery:mdns",
-    "discovery:public",
-    "platform",
+    "osp/msgs",
     "third_party/abseil",
     "third_party/boringssl",
     "third_party/googleurl",
     "third_party/jsoncpp",
-    "third_party/quiche",
     "third_party/tinycbor",
+    "tools/cddl($host_toolchain)",
     "util",
   ]
 
-  # TODO(issuetracker.google.com/236160471): CDDL does not compile on Windows.
+  # TODO(issuetracker.google.com/236163082): implement support on Windows.
   if (!is_win) {
     deps += [
+      "cast/streaming:receiver",
+      "cast/streaming:sender",
+      "discovery:dnssd",
+      "discovery:mdns",
+      "discovery:public",
       "osp",
-      "osp/msgs",
-      "tools/cddl($host_toolchain)",
-    ]
+      "platform",
 
+      # Quiche currently has an issue around logging macros on Windows.
+      "third_party/quiche",
+    ]
     if (!build_with_chromium) {
-      deps += [ "osp:osp_demo" ]
+      deps += [
+        "cast/standalone_sender:cast_sender",
+        "osp:osp_demo",
+        "third_party/protobuf:protoc($host_toolchain)",
+        "third_party/zlib",
+      ]
     }
   }
 
@@ -67,14 +73,6 @@ group("gn_all") {
   # implemented for certain operating systems.
   if (is_linux || is_mac || is_chromeos || is_android) {
     deps += [ "cast/standalone_receiver:cast_receiver" ]
-  }
-
-  if (!build_with_chromium) {
-    deps += [
-      "cast/standalone_sender:cast_sender",
-      "third_party/protobuf:protoc($host_toolchain)",
-      "third_party/zlib",
-    ]
   }
 }
 
@@ -99,16 +97,11 @@ openscreen_source_set("openscreen_unittests_all") {
       "discovery:unittests",
     ]
   }
-
-  # TODO(issuetracker.google.com/236160471): CDDL does not compile on Windows.
+  public_deps += [ "osp/msgs:unittests" ]
   if (!is_win) {
-    public_deps += [
-      "osp:unittests",
-      "osp/msgs:unittests",
-    ]
+    public_deps += [ "osp:unittests" ]
   }
 }
-
 if (!build_with_chromium) {
   openscreen_executable("openscreen_unittests") {
     testonly = true
@@ -119,7 +112,6 @@ if (!build_with_chromium) {
     ]
   }
 }
-
 if (!build_with_chromium && is_posix) {
   openscreen_source_set("e2e_tests_all") {
     testonly = true
@@ -127,12 +119,10 @@ if (!build_with_chromium && is_posix) {
       "cast/test:e2e_tests",
       "cast/test:make_crl_tests($host_toolchain)",
     ]
-
     if (is_linux) {
       public_deps += [ "cast/common:discovery_e2e_test" ]
     }
   }
-
   openscreen_executable("e2e_tests") {
     testonly = true
     deps = [
@@ -141,7 +131,6 @@ if (!build_with_chromium && is_posix) {
     ]
   }
 }
-
 if (!build_with_chromium) {
   openscreen_source_set("fuzzer_tests_all") {
     testonly = true

cast/standalone_receiver/BUILD.gn
@@ -72,6 +72,7 @@ openscreen_executable("cast_receiver") {
   deps = [
     ":cast_receiver_lib",
     "../../platform:standalone_impl",
+    "../../third_party/getopt",
     "../common:certificate_boringssl",
     "../common/certificate/proto:certificate_proto",
     "../receiver:channel",

cast/standalone_sender/BUILD.gn
@@ -32,6 +32,7 @@ if (!build_with_chromium) {
       "../../discovery:public",
       "../../platform",
       "../../platform:standalone_impl",
+      "../../third_party/getopt",
       "../../third_party/jsoncpp",
       "../../util",
       "../common:certificate_boringssl",

cast/streaming/impl/clock_drift_smoother.cc
@@ -69,8 +69,7 @@ void ClockDriftSmoother::Update(Clock::time_point now,
     }
 
     OSP_VLOG << "Local clock is ahead of the remote clock by: measured = "
-             << measured_offset << ", "
-             << "filtered = " << Current() << ".";
+             << measured_offset << ", " << "filtered = " << Current() << ".";
   }
 }
 

kaidokert/openscreen

commit e85b11d9defe2aecfde1182b9cf9007e927b1082

[Infra] Fix get_opt usage
The third party get_opt library that was added with
https://crrev.com/c/5490382 differs from POSIX getopt. It places the
optarg, optind, opterr, optopt global variables into the get_opt
namespace rather than into the top-level namespace. However, the
namespace specifier is not being used.

This fixes the usage of the get_opt global variables.

Bug: 236163082
Fixes: 4f27c4f16985 ("[Infra] Add Windows build support")
Change-Id: If744c113692b7b5c885ad26caa77e40cc7ea173a

cast/standalone_receiver/main.cc
@@ -150,16 +150,16 @@ int RunStandaloneReceiver(int argc, char* argv[]) {
                            nullptr)) != -1) {
     switch (ch) {
       case 'p':
-        private_key_path = optarg;
+        private_key_path = get_opt::optarg;
         break;
       case 'd':
-        developer_certificate_path = optarg;
+        developer_certificate_path = get_opt::optarg;
         break;
       case 'f':
-        friendly_name = optarg;
+        friendly_name = get_opt::optarg;
         break;
       case 'm':
-        model_name = optarg;
+        model_name = get_opt::optarg;
         break;
       case 'g':
         should_generate_credentials = true;
@@ -193,7 +193,7 @@ int RunStandaloneReceiver(int argc, char* argv[]) {
     return 1;
   }
 
-  const char* interface_name = argv[optind];
+  const char* interface_name = argv[get_opt::optind];
   OSP_CHECK(interface_name && strlen(interface_name) > 0)
       << "No interface name provided.";
 

cast/standalone_sender/main.cc
@@ -132,10 +132,11 @@ int StandaloneSenderMain(int argc, char* argv[]) {
                            nullptr)) != -1) {
     switch (ch) {
       case 'm':
-        max_bitrate = atoi(optarg);
+        max_bitrate = atoi(get_opt::optarg);
         if (max_bitrate < kMinRequiredBitrate) {
-          OSP_LOG_ERROR << "Invalid --max-bitrate specified: " << optarg
-                        << " is less than " << kMinRequiredBitrate;
+          OSP_LOG_ERROR << "Invalid --max-bitrate specified: "
+                        << get_opt::optarg << " is less than "
+                        << kMinRequiredBitrate;
           LogUsage(argv[0]);
           return 1;
         }
@@ -144,7 +145,7 @@ int StandaloneSenderMain(int argc, char* argv[]) {
         should_loop_video = false;
         break;
       case 'd':
-        developer_certificate_path = optarg;
+        developer_certificate_path = get_opt::optarg;
         break;
       case 'a':
         use_android_rtp_hack = true;
@@ -162,14 +163,14 @@ int StandaloneSenderMain(int argc, char* argv[]) {
         LogUsage(argv[0]);
         return 1;
       case 'c':
-        auto specified_codec = StringToVideoCodec(optarg);
+        auto specified_codec = StringToVideoCodec(get_opt::optarg);
         if (specified_codec.is_value() &&
             (specified_codec.value() == VideoCodec::kVp8 ||
              specified_codec.value() == VideoCodec::kVp9 ||
              specified_codec.value() == VideoCodec::kAv1)) {
           codec = specified_codec.value();
         } else {
-          OSP_LOG_ERROR << "Invalid --codec specified: " << optarg
+          OSP_LOG_ERROR << "Invalid --codec specified: " << get_opt::optarg
                         << " is not one of: vp8, vp9, av1.";
           LogUsage(argv[0]);
           return 1;
@@ -183,12 +184,12 @@ int StandaloneSenderMain(int argc, char* argv[]) {
   // The second to last command line argument must be one of: 1) the network
   // interface name or 2) a specific IP address (port is optional). The last
   // argument must be the path to the file.
-  if (optind != (argc - 2)) {
+  if (get_opt::optind != (argc - 2)) {
     LogUsage(argv[0]);
     return 1;
   }
-  const char* const iface_or_endpoint = argv[optind++];
-  const char* const path = argv[optind];
+  const char* const iface_or_endpoint = argv[get_opt::optind++];
+  const char* const path = argv[get_opt::optind];
 
   std::unique_ptr<TrustStore> cast_trust_store;
   if (!developer_certificate_path.empty()) {

osp/demo/osp_demo.cc
@@ -657,8 +657,8 @@ InputArgs GetInputArgs(int argc, char** argv) {
     }
   }
 
-  if (optind < argc) {
-    args.friendly_server_name = argv[optind];
+  if (get_opt::optind < argc) {
+    args.friendly_server_name = argv[get_opt::optind];
   }
 
   return args;
e287ee2a5c87feef1accc1435a32dae0448ce765
Kaido Kert <kaidokert@google.com>
2025-11-30 14:24:01 -0800

    [WIP][Draft] Windows build of Openscreen
    Initial pass of Windos platform support. This compiles with MSVC.
    A few notes:
    - For review convenience, i've added a is_experimental_win_build
      build flag, that works around large GN code chunks being
      re-formatted. This should be temporary and is_win blocks should
      get collapsed.
    - Because of MSVC compilation, there are a few small C++ changes
      to make it build all the code.
    - A lot of this was written and verified by Gemini-CLI
    - There are a bit hacky changes in //build needed to make it fully
      build
    
    Bug: 236163082
    Bug: 460509759
    Change-Id: Iaf9af14b038854c2877583abf680c42604b6d403

---
+++ platform/impl/socket_handle_win.cc
@@ -0,0 +1,19 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/impl/socket_handle_win.h"
+
+namespace openscreen {
+
+SocketHandle::SocketHandle(SOCKET descriptor) : handle(descriptor) {}
+
+size_t SocketHandleHash::operator()(const SocketHandle& handle) const {
+  return static_cast<size_t>(handle.handle);
+}
+
+bool operator==(const SocketHandle& lhs, const SocketHandle& rhs) {
+  return lhs.handle == rhs.handle;
+}
+
+}  // namespace openscreen
---
+++ platform/impl/udp_socket_reader_win.h
@@ -0,0 +1,49 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PLATFORM_IMPL_UDP_SOCKET_READER_WIN_H_
+#define PLATFORM_IMPL_UDP_SOCKET_READER_WIN_H_
+
+#include <map>
+#include <mutex>
+#include <vector>
+
+#include "platform/api/task_runner.h"
+#include "platform/api/time.h"
+#include "platform/impl/socket_handle.h"
+#include "platform/impl/socket_handle_waiter.h"
+#include "platform/impl/udp_socket_win.h"
+
+namespace openscreen {
+
+class UdpSocketReaderWin : public SocketHandleWaiter::Subscriber {
+ public:
+  using SocketHandleRef = SocketHandleWaiter::SocketHandleRef;
+
+  explicit UdpSocketReaderWin(SocketHandleWaiter& waiter);
+  ~UdpSocketReaderWin() override;
+
+  void OnCreate(UdpSocketWin* socket);
+  void OnDestroy(UdpSocketWin* socket);
+
+  // SocketHandleWaiter::Subscriber overrides.
+  void ProcessReadyHandle(SocketHandleRef handle, uint32_t flags) override;
+  bool HasPendingWrite(SocketHandleRef handle) override;
+
+  OSP_DISALLOW_COPY_AND_ASSIGN(UdpSocketReaderWin);
+
+ protected:
+  bool IsMappedReadForTesting(UdpSocketWin* socket) const;
+
+ private:
+  void OnDelete(UdpSocketWin* socket, bool disable_locking_for_testing = false);
+
+  std::vector<UdpSocketWin*> sockets_;
+  mutable std::mutex mutex_;
+  SocketHandleWaiter& waiter_;
+};
+
+}  // namespace openscreen
+
+#endif  // PLATFORM_IMPL_UDP_SOCKET_READER_WIN_H_
